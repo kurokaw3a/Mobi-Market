aacab72426f8e5319477cb69dd578f3926d24aa7
Timur <kamaldinov321@gmail.com>
2023-12-14 21:16:19 +0600

    ui-sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,4 @@
-import Sidebar from './components/UI/Sidebar/Sidebar'
-
 const App = () => {
-  return (
-    <Sidebar
-      name='Тимур'
-      nickname='kurokaw3a'
-      avatar='https://lumiere-a.akamaihd.net/v1/images/607598d0230e6a00018e21b2-image_354b1b56.jpeg?region=0,0,1536,864'
-    />
-  )
+  return <div />
 }
 export default App
